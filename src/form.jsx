dcb6c8285d845a3e579c8d7a5a17689c1414aa3d
rokaya magdi <rokaiamagdi@gmail.com>
2021-01-24 11:30:13 +0200

    using state Hook in stateless component

--- src/form.jsx
+++ src/form.jsx
@@ -1,38 +1,34 @@
 //import React from 'react';
-import React, { Component, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 const Form = () => {
-  const [name, setName] = useState('roka')
-  function handelChange(e) {
+  const [name, setName] = useState('rokaya')
+  const [surname, setSurname] = useState('magdi')
+
+  function handelChangeName(e) {
     setName(e.target.value)
   }
+  function handelChangeSurname(e) {
+    setSurname(e.target.value)
+  }
+  useEffect(() => {
+    document.title = name + ' ' + surname
+  })
   return (
-    <div>
+    <div className="form-group col-md-4 mt-3">
       <label>My name</label>
-      <input value={name}
-        onChange={handelChange} />
+      <input
+        class="form-control"
+        value={name}
+        onChange={handelChangeName} />
+      <label className="mt-4">sur name</label>
+      <input
+        class="form-control"
+        value={surname}
+        onChange={handelChangeSurname} />
     </div>
   );
 }
-// class Form extends Component {
-//   state = {
-//     name: "rokaya"
-//   }
-//   handelChange = (e) => {
-//     this.setState({ name: e.target.value })
-//   }
-//   render() {
-//     return (
-//       <div>
 
-//         <label>My name</label>
-//         <input
-//           value={this.state.name}
-//           onChange={this.handelChange}
-//         />
-//       </div>
-//     );
-//   }
-// }
 
 export default Form;
 
